5130037ca469aaadb8b3e6476e00e041361694cb
Suchi Goel <suchig946@gmail.com>
2025-12-25 13:42:53 +0530

    Time: 174 ms (30.85%) | Memory: 107.7 MB (97.87%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        
+        long long ans = 0;
+        for (int i = 0; i < k; i++) {
+            int curr = happiness[i] - i;
+            if (curr <= 0) break;
+            ans += curr;
+        }
+        return ans;
+    }
+};
